42e0c24df8f36df741102d700cc3ec472e9c0322
Sean McNamara <sean.mcnamara@ubc.ca>
2015-08-12 11:58:36 -0700

    Getters, setters and constructors

--- src/OpenLibrary/Metadata/Schemas/EDM/ProvidedCHO.php
+++ src/OpenLibrary/Metadata/Schemas/EDM/ProvidedCHO.php
@@ -6,6 +6,26 @@
     trait ProvidedCHO
     {
         /**
+         * @var string
+         */
+        private $uri = "http://www.europeana.eu/schemas/edm/ProvidedCHO";
+
+        /**
+         * @var bool|string
+         */
+        private $label = "ProvidedCHO";
+
+        /**
+         * @var string
+         */
+        private $name = "edm:ProvidedCHO";
+
+        /**
+         * @var string
+         */
+        private $description = "A Europeana Data Model Class";
+
+        /**
          * @var Schemas\DCTerms\Properties\IsReferencedBy
          */
         protected $CatalogueRecord;
@@ -24,4 +44,147 @@
          * @var Schemas\DCTerms\Properties\Temporal
          */
         protected $Time;
+
+        /**
+         * ProvidedCHO constructor.
+         * @param Schemas\DCTerms\Properties\IsReferencedBy $CatalogueRecord
+         * @param Properties\CurrentLocation $RBSCLocation
+         * @param Properties\IsDerivativeOf $Translation
+         * @param Schemas\DCTerms\Properties\Temporal $Time
+         */
+        public function __construct(Schemas\DCTerms\Properties\IsReferencedBy $CatalogueRecord, Properties\CurrentLocation $RBSCLocation, Properties\IsDerivativeOf $Translation, Schemas\DCTerms\Properties\Temporal $Time)
+        {
+            $this->CatalogueRecord = $CatalogueRecord;
+            $this->RBSCLocation = $RBSCLocation;
+            $this->Translation = $Translation;
+            $this->Time = $Time;
+        }
+
+        /**
+         * @return bool|string
+         */
+        public function getLabel()
+        {
+            return $this->label;
+        }
+
+        /**
+         * @param bool|string $label
+         */
+        public function setLabel($label)
+        {
+            $this->label = $label;
+        }
+
+        /**
+         * @return string
+         */
+        public function getUri()
+        {
+            return $this->uri;
+        }
+
+        /**
+         * @param string $uri
+         */
+        public function setUri($uri)
+        {
+            $this->uri = $uri;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        /**
+         * @param string $name
+         */
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+        /**
+         * @param string $description
+         */
+        public function setDescription($description)
+        {
+            $this->description = $description;
+        }
+
+        /**
+         * @return Schemas\DCTerms\Properties\IsReferencedBy
+         */
+        public function getCatalogueRecord()
+        {
+            return $this->CatalogueRecord;
+        }
+
+        /**
+         * @param Schemas\DCTerms\Properties\IsReferencedBy $CatalogueRecord
+         */
+        public function setCatalogueRecord($CatalogueRecord)
+        {
+            $this->CatalogueRecord = $CatalogueRecord;
+        }
+
+        /**
+         * @return Properties\CurrentLocation
+         */
+        public function getRBSCLocation()
+        {
+            return $this->RBSCLocation;
+        }
+
+        /**
+         * @param Properties\CurrentLocation $RBSCLocation
+         */
+        public function setRBSCLocation($RBSCLocation)
+        {
+            $this->RBSCLocation = $RBSCLocation;
+        }
+
+        /**
+         * @return Properties\IsDerivativeOf
+         */
+        public function getTranslation()
+        {
+            return $this->Translation;
+        }
+
+        /**
+         * @param Properties\IsDerivativeOf $Translation
+         */
+        public function setTranslation($Translation)
+        {
+            $this->Translation = $Translation;
+        }
+
+        /**
+         * @return Schemas\DCTerms\Properties\Temporal
+         */
+        public function getTime()
+        {
+            return $this->Time;
+        }
+
+        /**
+         * @param Schemas\DCTerms\Properties\Temporal $Time
+         */
+        public function setTime($Time)
+        {
+            $this->Time = $Time;
+        }
     }
--- src/OpenLibrary/Metadata/Schemas/EDM/WebResource.php
+++ src/OpenLibrary/Metadata/Schemas/EDM/WebResource.php
@@ -13,6 +13,26 @@
     trait WebResource
     {
         /**
+         * @var string
+         */
+        private $uri = "http://www.europeana.eu/schemas/edm/WebResource";
+
+        /**
+         * @var bool|string
+         */
+        private $label = "WebResource";
+
+        /**
+         * @var string
+         */
+        private $name = "edm:WebResource";
+
+        /**
+         * @var string
+         */
+        private $description = "A Europeana Data Model Class";
+
+        /**
          * @var Schemas\DC\Properties\Format
          */
         protected $FileFormat;
@@ -31,4 +51,147 @@
          * @var Schemas\EDM\Properties\IsShownAt
          */
         protected $IsShownAt;
+
+        /**
+         * WebResource constructor.
+         * @param Schemas\DC\Properties\Format $FileFormat
+         * @param Schemas\DCTerms\Properties\Rights $Rights
+         * @param Schemas\DCTerms\Properties\Issued $DateIssued
+         * @param Properties\IsShownAt $IsShownAt
+         */
+        public function __construct(Schemas\DC\Properties\Format $FileFormat, Schemas\DCTerms\Properties\Rights $Rights, Schemas\DCTerms\Properties\Issued $DateIssued, Properties\IsShownAt $IsShownAt)
+        {
+            $this->FileFormat = $FileFormat;
+            $this->Rights = $Rights;
+            $this->DateIssued = $DateIssued;
+            $this->IsShownAt = $IsShownAt;
+        }
+
+        /**
+         * @return string
+         */
+        public function getUri()
+        {
+            return $this->uri;
+        }
+
+        /**
+         * @param string $uri
+         */
+        public function setUri($uri)
+        {
+            $this->uri = $uri;
+        }
+
+        /**
+         * @return bool|string
+         */
+        public function getLabel()
+        {
+            return $this->label;
+        }
+
+        /**
+         * @param bool|string $label
+         */
+        public function setLabel($label)
+        {
+            $this->label = $label;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        /**
+         * @param string $name
+         */
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+        /**
+         * @param string $description
+         */
+        public function setDescription($description)
+        {
+            $this->description = $description;
+        }
+
+        /**
+         * @return Schemas\DC\Properties\Format
+         */
+        public function getFileFormat()
+        {
+            return $this->FileFormat;
+        }
+
+        /**
+         * @param Schemas\DC\Properties\Format $FileFormat
+         */
+        public function setFileFormat($FileFormat)
+        {
+            $this->FileFormat = $FileFormat;
+        }
+
+        /**
+         * @return Schemas\DCTerms\Properties\Rights
+         */
+        public function getRights()
+        {
+            return $this->Rights;
+        }
+
+        /**
+         * @param Schemas\DCTerms\Properties\Rights $Rights
+         */
+        public function setRights($Rights)
+        {
+            $this->Rights = $Rights;
+        }
+
+        /**
+         * @return Schemas\DCTerms\Properties\Issued
+         */
+        public function getDateIssued()
+        {
+            return $this->DateIssued;
+        }
+
+        /**
+         * @param Schemas\DCTerms\Properties\Issued $DateIssued
+         */
+        public function setDateIssued($DateIssued)
+        {
+            $this->DateIssued = $DateIssued;
+        }
+
+        /**
+         * @return Properties\IsShownAt
+         */
+        public function getIsShownAt()
+        {
+            return $this->IsShownAt;
+        }
+
+        /**
+         * @param Properties\IsShownAt $IsShownAt
+         */
+        public function setIsShownAt($IsShownAt)
+        {
+            $this->IsShownAt = $IsShownAt;
+        }
     }
